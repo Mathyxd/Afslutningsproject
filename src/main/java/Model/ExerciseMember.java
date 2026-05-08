665448b79f4702d341ddbed9aad36ed1a681a901
Mathyxd <mathias3730@gmail.com>
2026-05-08 12:42:30 +0200

    get/set

--- src/main/java/Model/ExerciseMember.java
+++ src/main/java/Model/ExerciseMember.java
@@ -4,16 +4,5 @@ public class ExerciseMember extends Member {
     public ExerciseMember(String name, int age, int memberID, boolean activeMember) {
         super(name, age, memberID, activeMember);
     }
-    @Override
-    public double calculateFee() {
-        if (!isActiveMember()) {
-            return 250;
-        } else if (isJunior()) {
-            return 800;
-        } else if (isSeniorOld()) {
-            return 1500 * 0.75;
-        } else {
-            return 1500;
-        }
-    }
 }
+
