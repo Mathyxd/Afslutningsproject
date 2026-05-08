111f6c13f8fab3a84aafbeaa27bb1b87323f47c0
holmganga <siggi.sejr@gmail.com>
2026-05-08 13:06:25 +0200

    små ting

--- src/main/java/Model/Member.java
+++ src/main/java/Model/Member.java
@@ -1,10 +1,16 @@
 package Model;
 
+import Validation.InputValidator;
+
+import static Validation.InputValidator.*;
+
 public class Member {
     String name;
     int age;
 
     public Member(String name, int age){
+        validateName(name);
+        validateAge(age);
         this.name = name;
         this.age = age;
     }
