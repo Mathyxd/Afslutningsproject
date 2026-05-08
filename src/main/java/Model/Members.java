bb52d9acbe4f2a3880754337c5d1ff390f37437a
holmganga <siggi.sejr@gmail.com>
2026-05-08 13:25:22 +0200

    Members oprettet med constructor

--- src/main/java/Model/Members.java
+++ src/main/java/Model/Members.java
@@ -1,4 +1,11 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Members {
+    ArrayList<Member> members;
+
+    public Members(ArrayList<Member> members){
+        this.members = members;
+    }
 }
