b1d2bf98f0543588e5d77d7138c4cc8a3e540d97
Mathyxd <mathias3730@gmail.com>
2026-05-08 13:07:32 +0200

    competitivemember

--- src/main/java/Model/CompetitiveMember.java
+++ src/main/java/Model/CompetitiveMember.java
@@ -2,4 +2,9 @@ package Model;
 
 public class CompetitiveMember extends Member {
     private Discipline discipline;
+
+    public CompetitiveMember(String name, int age, int memberID, boolean activeMember, Discipline discipline) {
+        super(name, age, memberID, activeMember);
+        this.discipline = discipline;
+    }
 }
