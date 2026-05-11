47b09711abbffd6f18fcd699a801e17d151b5eb4
holmganga <siggi.sejr@gmail.com>
2026-05-11 13:05:19 +0200

    InputValidatorTest rettet ift Member klassen og kommentarer tilføjet

--- src/test/java/validation/InputValidatorTest.java
+++ src/test/java/validation/InputValidatorTest.java
@@ -1,6 +1,7 @@
 package validation;
 
 import exceptionhandler.ValidationException;
+import model.ExerciseMember;
 import model.Member;
 import org.junit.jupiter.api.Test;
 
@@ -8,13 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InputValidatorTest {
 
+
+    // Tester, om validateName() metoden smider en "ValidationException", hvis et ExerciseMember oprettes med et tomt navn
     @Test
     void validateName() {
-        assertThrows(ValidationException.class, () -> new Member(null, 6));
+        assertThrows(ValidationException.class,
+                () -> new ExerciseMember(null, 6, 1234, true));
     }
 
+    // Tester, om validateAge() metoden smider en "ValidationException", hvis et ExerciseMember oprettes med en
+    // alder på 0 eller mindre
     @Test
     void validateAge() {
-        assertThrows(ValidationException.class, () -> new Member("Alice", -6));
+        assertThrows(ValidationException.class,
+                () -> new ExerciseMember("Alice", -6, 123, false));
     }
 }
