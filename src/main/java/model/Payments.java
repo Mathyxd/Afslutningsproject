253b809cb9c677e249adc8633947c099766e4921
Jenny <euro1000@stud.ek.dk>
2026-05-13 11:05:53 +0200

    feauture Payments - ArrayList

--- src/main/java/model/Payments.java
+++ src/main/java/model/Payments.java
@@ -1,4 +1,11 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Payments {
+    ArrayList<Payment> payments;
+
+    public Payments(ArrayList<Payment> payments){
+        this.payments = payments;
+    }
 }
